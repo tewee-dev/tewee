57035e74042684ceca4e36ceaa86909027327248
Evan Yang <quantbit@126.com>
2026-06-20 13:25:21 +0800

    Delete `__all__` in module

--- tewee/__init__.py
+++ tewee/__init__.py
@@ -1,11 +1,5 @@
 """Tewee module - simplify metadata & dataclass handling, clean & developer-friendly."""
 
-from .api import EmailStr, Only
+from .api import EmailStr as EmailStr
+from .api import Only as Only
 from .version import __version__, __version_tuple__
-
-__all__ = [
-    "EmailStr",
-    "Only",
-    "__version__",
-    "__version_tuple__",
-]
