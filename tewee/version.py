5d745fc972bd29f673831cfb5ca358c37e7d8de3
Evan Yang <quantbit@126.com>
2026-06-21 12:01:12 +0800

    Add `__commit_id__` metadata

--- tewee/version.py
+++ tewee/version.py
@@ -1,2 +1,3 @@
-__version__: str = "0.0.5"
-__version_tuple__: tuple[int, int, int] = (0, 0, 5)
+__version__: str = "0.0.6"
+__version_tuple__: tuple[int, int, int] = (0, 0, 6)
+__commit_id__: str | None = None
